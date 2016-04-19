b5cedd79f4bd451c5d7fa944d24156c291c643cb
Keaton Burleson <keaton.burleson@me.com>
2016-04-19 10:09:33 -0500

    now it fetches a list

--- preferences/Preferences.mm
+++ preferences/Preferences.mm
@@ -1,88 +1,91 @@
 #import <Preferences/Preferences.h>
 #import <EventKit/EventKit.h>
 #import <UIKit/UIKit.h>
-@interface PreferencesListController: PSListController <UITableViewDataSource, UITableViewDelegate> {
+@interface PreferencesListController: PSViewController <UITableViewDataSource, UITableViewDelegate> {
+
+    NSString *_path;
+    NSArray *_paths;
+    NSMutableArray *_enabledPaths;
+    NSMutableArray *enabledIdentifiers;
+    NSMutableArray *disabledIdentifiers;
+    NSString *_notificationName;
+    NSString *_settingsFile;
+    NSString *enabledKey;
+    NSString *disabledKey;
     
 }
 @property(strong, nonatomic) NSMutableArray *reminders;
 @property (strong, nonatomic) EKEventStore *store;
 @property (strong, nonatomic) NSMutableArray *events;
 @property (strong, nonatomic) NSMutableArray *completed;
+@property (strong, nonatomic) NSMutableArray *arrayTag;
+
 @end
 
 @implementation PreferencesListController
-/*- (id)specifiers {
-	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"Preferences" target:self] retain];
-	}
-	return _specifiers;
-}*/
 
+@synthesize arrayTag;
 -(void)viewDidLoad{
     [super viewDidLoad];
-    NSSortDescriptor *sortDescriptor;
-    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"priority" ascending:YES] ;
-    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-    
-    NSPredicate *predicate = [self.store predicateForIncompleteRemindersWithDueDateStarting: nil ending: nil calendars: nil];
-    
-    NSPredicate *completePredicate = [self.store predicateForCompletedRemindersWithCompletionDateStarting: nil ending: nil calendars: nil];
-    
-    [self.store fetchRemindersMatchingPredicate:predicate completion:^(NSArray *completed) {
-        self.events = [[completed sortedArrayUsingDescriptors: sortDescriptors] mutableCopy];
-        
-    }];
-    [self.store fetchRemindersMatchingPredicate:completePredicate completion:^(NSArray *incompleted) {
-        self.completed = [[incompleted sortedArrayUsingDescriptors: sortDescriptors] mutableCopy];
-        
-    }];
-    
-    if(self.store == nil){
-        self.store = [[EKEventStore alloc] init];
-    }
-    NSArray *calendars = [self.store calendarsForEntityType:EKEntityTypeEvent];
-
-    for (EKCalendar *r in calendars) {
-        [self.reminders addObject: r.title];
-    }
     UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 0.0f, 0.0f) style:UITableViewStyleGrouped];
     tableView.dataSource = self;
     tableView.delegate = self;
     self.view = tableView;
+    [self refreshData];
+    [self.navigationItem setTitle: @"Made with Bananas"];
+    NSLog(@"potato windows");
+    arrayTag = [NSMutableArray array];
     
+    [(UITableView *)self.view reloadData];
     
 }
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
-    return 2;
+-(void)viewDidAppear: (BOOL) animated{
+    [super viewDidAppear: true];
+    [self refreshData];
+    [(UITableView *)self.view reloadData];
 }
-- (NSInteger)tableView:(UITableView *)tableView
- numberOfRowsInSection:(NSInteger)section {
-    if (section == 0) {
-        return 2;
-    }else{
-        return [self.reminders count];
-    }
+-(void)refreshData{
+
+    EKEventStore * eventStore = [[EKEventStore alloc] init];
+    EKEntityType type = EKEntityTypeReminder;
+    self.reminders = [[eventStore calendarsForEntityType:type] mutableCopy];
+
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)table {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section {
+    return self.reminders.count;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"] ?: [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"] autorelease];
+
+    EKCalendar *cal = self.reminders[indexPath.row];
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    cell.textLabel.text = cal.title;
+    
+    cell.tag = indexPath.row;
+    NSString *strCellTag = [NSString stringWithFormat:@"%ld", (long)cell.tag];
+    if(![arrayTag containsObject:strCellTag])
+    {
+        [arrayTag addObject:strCellTag];
     }
-    if(indexPath.section == 0){
-    // Configure the cell.
-    cell.textLabel.text = @"Thanks ryan!";
+
     return cell;
-    }else{
-        cell.textLabel.text = self.reminders[indexPath.row];
-         return cell;
-    }
 }
 
-
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    NSString *stringToMove = [arrayTag objectAtIndex:sourceIndexPath.row];
+    [arrayTag removeObjectAtIndex:sourceIndexPath.row];
+    [arrayTag insertObject:stringToMove atIndex:destinationIndexPath.row];
+}
 
 @end
 
-// vim:ft=objc
+
